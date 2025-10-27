2401e0048c89e8ec8d40eef34958a938ca3a37c9
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2025-10-27 12:50:37 +0900

    Add extract.sh to create data for plotting

---
+++ log/extract.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+sleep_usec=100000
+
+port=$(head -n 1 net.txt | awk '{print $3}' | awk -F '.' '{print $5}')
+start_time=$(head -n 1 total.log | awk '{print $1}')
+
+grep -E '(now sleep|readn\(\) start)' total.log | sed -e "s/now sleeping $sleep_usec usec/0/" -e 's/readn() start.*$/1/' > program.activity
+grep -E "IP 192.168.10.202.$port > 192.168.10.17.24: Flags \[.\], ack" total.log | sed -e 's/,//g' | awk '{print $1, $9}' > ack.activity
+
+tcpdumpdiff -s $start_time program.activity > program.activity.log
+tcpdumpdiff -s $start_time ack.activity > ack.activity.log
+
+grep -E "IP 192.168.10.17.24 > 192.168.10.202.$port:" total.log | grep -v mss | grep -v none | sed -e 's/,//g' -e 's/:/ /g' > data.activity
+tcpdumpdiff -s $start_time data.activity > data.activity.log
+
+fgrep 'readn() start' total.log | sed -e 's/(//g'  -e s'/)//g' > rcvbuf.activity
+tcpdumpdiff -s $start_time rcvbuf.activity > rcvbuf.activity.log
