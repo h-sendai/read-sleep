b9a058f09c019c33c07bb772eb6234d65941d22b
Hiroshi Sendai <hiroshi.sendai@kek.jp>
2025-10-27 14:06:14 +0900

    Change filename

--- log/extract.sh
+++ log/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-sleep_usec=100000
+sleep_usec=85000
 
 port=$(head -n 1 net.txt | awk '{print $3}' | awk -F '.' '{print $5}')
 start_time=$(head -n 1 total.log | awk '{print $1}')
@@ -11,8 +11,8 @@ grep -E "IP 192.168.10.202.$port > 192.168.10.17.24: Flags \[.\], ack" total.log
 tcpdumpdiff -s $start_time program.activity > program.activity.log
 tcpdumpdiff -s $start_time ack.activity > ack.activity.log
 
-grep -E "IP 192.168.10.17.24 > 192.168.10.202.$port:" total.log | grep -v mss | grep -v none | sed -e 's/,//g' -e 's/:/ /g' > data.activity
-tcpdumpdiff -s $start_time data.activity > data.activity.log
+grep -E "IP 192.168.10.17.24 > 192.168.10.202.$port:" total.log | grep -v mss | grep -v none | sed -e 's/,//g' -e 's/:/ /g' > packet.activity
+tcpdumpdiff -s $start_time packet.activity > packet.activity.log
 
 fgrep 'readn() start' total.log | sed -e 's/(//g'  -e s'/)//g' > rcvbuf.activity
 tcpdumpdiff -s $start_time rcvbuf.activity > rcvbuf.activity.log
